7b0ba78817f5beaf2b5a9c19e3e89275847e8ebe
Richard Leishman <mrl22@users.noreply.github.com>
2020-10-26 19:11:50 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -295,7 +295,7 @@ add_filter( 'login_headerurl', 'custom_loginlogo_url' );
 ----------------------------------------------------------------------------------------------------*/
 
 //function my_login_stylesheet() {
-//	wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/style-login.css' );
+//	wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/build/style-login.css' );
 //}
 //add_action( 'login_enqueue_scripts', 'my_login_stylesheet' );
 
